98b7310d773849e7bfbd64d232a235aae34ec81e
Albert Duran <albertduranll@gmail.com>
2022-03-02 19:50:21 +0100

    :construction: Main scene created

--- src/script.js
+++ src/script.js
@@ -2,119 +2,82 @@ import './style.css'
 import * as THREE from 'three'
 import { ARButton } from 'three/examples/jsm/webxr/ARButton.js'
 
-// // Canvas
-// const canvas = document.querySelector('canvas.webgl')
 
-// // Scene
-// const scene = new THREE.Scene()
+/**
+ * Sizes
+ */
+ const sizes = {}
+ sizes.width = window.innerWidth
+ sizes.height = window.innerHeight
+ 
+ window.addEventListener('resize', () =>
+ {
+     // Save sizes
+     sizes.width = window.innerWidth
+     sizes.height = window.innerHeight
+ 
+     // Update camera
+     camera.aspect = sizes.width / sizes.height
+     camera.updateProjectionMatrix()
+ 
+     // Update renderer
+     renderer.setSize(sizes.width, sizes.height)
+ })
+ 
+ /**
+  * Environnements
+  */
+ // Scene
+ const scene = new THREE.Scene()
+ 
+ // Camera
+ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+ camera.position.z = 3
+ scene.add(camera)
+ 
+ // Test
+ const cube = new THREE.Mesh(new THREE.BoxBufferGeometry(1, 1, 1), new THREE.MeshNormalMaterial())
+ scene.add(cube)
+ 
+ // Renderer
+ const renderer = new THREE.WebGLRenderer({
+     canvas: document.querySelector('.webgl')
+ })
+ renderer.setPixelRatio(window.devicePixelRatio)
+ renderer.setSize(sizes.width, sizes.height)
+ 
+/**
+ * AR BUTTON
+ */
+ document.body.appendChild( ARButton.createButton( renderer ) );
+ const controller = renderer.xr.getController( 0 );
 
-// /**
-//  * Object
-//  */
-// const geometry = new THREE.BoxGeometry(1, 1, 1)
-// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-// const mesh = new THREE.Mesh(geometry, material)
-// scene.add(mesh)
+ function onSelect() {
+ 
+     // var material = new THREE.MeshPhongMaterial( { color: 0xffffff * Math.random() } );
+     // var mesh = new THREE.Mesh( geometry, material );
+     // mesh.position.set( 0, 0, - 0.3 ).applyMatrix4( controller.matrixWorld );
+     // mesh.quaternion.setFromRotationMatrix( controller.matrixWorld );
+     // scene.add( mesh );
+     console.log('AR selection');
+ 
+ }
+ 
+ controller.addEventListener( 'select', onSelect );
+ scene.add( controller );
 
-// /**
-//  * Sizes
-//  */
-// const sizes = {
-//     width: 800,
-//     height: 600
-// }
-
-// /**
-//  * Camera
-//  */
-// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-// camera.position.z = 3
-// scene.add(camera)
-
-// /**
-//  * Renderer
-//  */
-// const renderer = new THREE.WebGLRenderer({
-//     canvas: canvas
-// })
-// renderer.setSize(sizes.width, sizes.height)
-// renderer.render(scene, camera)
-
-var container;
-			var camera, scene, renderer;
-			var controller;
-
-			init();
-			animate();
-
-			function init() {
-
-				container = document.createElement( 'div' );
-				document.body.appendChild( container );
-
-				scene = new THREE.Scene();
-
-				camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 20 );
-
-				var light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
-				light.position.set( 0.5, 1, 0.25 );
-				scene.add( light );
-
-				//
-
-				renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
-				renderer.setPixelRatio( window.devicePixelRatio );
-				renderer.setSize( window.innerWidth, window.innerHeight );
-				renderer.xr.enabled = true;
-				container.appendChild( renderer.domElement );
-
-				//
-
-				document.body.appendChild( ARButton.createButton( renderer ) );
-
-				//
-
-				var geometry = new THREE.CylinderBufferGeometry( 0, 0.05, 0.2, 32 ).rotateX( Math.PI / 2 );
-
-				function onSelect() {
-
-					var material = new THREE.MeshPhongMaterial( { color: 0xffffff * Math.random() } );
-					var mesh = new THREE.Mesh( geometry, material );
-					mesh.position.set( 0, 0, - 0.3 ).applyMatrix4( controller.matrixWorld );
-					mesh.quaternion.setFromRotationMatrix( controller.matrixWorld );
-					scene.add( mesh );
-
-				}
-
-				controller = renderer.xr.getController( 0 );
-				controller.addEventListener( 'select', onSelect );
-				scene.add( controller );
-
-				//
-
-				window.addEventListener( 'resize', onWindowResize, false );
-
-			}
-
-			function onWindowResize() {
-
-				camera.aspect = window.innerWidth / window.innerHeight;
-				camera.updateProjectionMatrix();
-
-				renderer.setSize( window.innerWidth, window.innerHeight );
-
-			}
-
-			//
-
-			function animate() {
-
-				renderer.setAnimationLoop( render );
-
-			}
-
-			function render() {
-
-				renderer.render( scene, camera );
-
-			}
+ /**
+  * Loop
+  */
+ const loop = () =>
+ {
+     // Update
+     cube.rotation.y += 0.01
+ 
+     // Render
+     renderer.render(scene, camera)
+ 
+     // Keep looping
+     window.requestAnimationFrame(loop)
+ }
+ loop()
